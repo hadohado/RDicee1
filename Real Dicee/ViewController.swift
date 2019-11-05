f1d412250dba4aeef22014d52236511de49037bc
hd <ha_do@yahoo.com>
2019-11-05 01:23:36 -0800

    can only display 1 dice

--- Real Dicee/ViewController.swift
+++ Real Dicee/ViewController.swift
@@ -75,14 +75,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
 
                 // I add nov-5-2019    add previous dices onto scene
-                if let diceNodeOld = diceScene.rootNode.childNode(withName: "Dice", recursively: false) {
+                if let diceNodeOld = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    if let mycount = diceLocations?.count {
+                        print("-------- mycount = ", mycount)
+                    for i in 0...(mycount - 1) {
+                        print("--------x = ", diceLocations?[i].diceLocx )
+                        diceNodeOld.position = SCNVector3(
+                            x: (diceLocations?[i].diceLocx)!,
+                            y: (diceLocations?[i].diceLocy)!,
+                            z: (diceLocations?[i].diceLocz)!
+                        )
+                        sceneView.scene.rootNode.addChildNode(diceNodeOld)
+                    }
+                    }
                     
-                    diceNodeOld.position = SCNVector3(
-                        x: (diceLocations?[0].diceLocx)!,
-                        y: (diceLocations?[0].diceLocy)!,
-                        z: (diceLocations?[0].diceLocz)!
-                    )
-                    sceneView.scene.rootNode.addChildNode(diceNodeOld)
+//                    diceNodeOld.position = SCNVector3(
+//                        x: (diceLocations?[0].diceLocx)!,
+//                        y: (diceLocations?[0].diceLocy)!,
+//                        z: (diceLocations?[0].diceLocz)!
+//                    )
+//                    sceneView.scene.rootNode.addChildNode(diceNodeOld)
+                    
+                    
+                    
                 } // I add nov-5-2019    add previous dices onto scene
                 
                 
